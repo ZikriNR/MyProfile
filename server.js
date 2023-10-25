4cc787cf5dbde3df9da7ae85d59626d7df870662
Zikri Nur Rahman <zikrinr16@gmail.com>
2023-10-25 11:55:50 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict'
+const express = require('express')
+const app = express()
+
+app.use(express.static('src'))
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/src/index.html');
+});
+
+const PORT = process.env.PORT || 8000
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
